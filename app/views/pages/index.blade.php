53b231148a175e57a08d14ff3abe300e56a47c22
Alexander Pape <a.pape@paneon.de>
2014-04-15 23:57:49 +0200

    Prefill XING Link if entered in the database

--- app/views/pages/index.blade.php
+++ app/views/pages/index.blade.php
@@ -1,6 +1,13 @@
 
 @section('content')
-<h1>Willkommen {{$userName}}</h1>
+<div class="row">
+    <div class="col-md-11">
+        <h1>Willkommen {{$userName}}</h1>
+    </div>
+    <div class="col-md-1 pull-right">
+        <a href="/{{$serial}}/{{$fmId}}"><i class="glyphicon glyphicon-refresh"></i></a>
+    </div>
+</div>
     {{ Form::open(array(
         'url' => route(
             'form', array(
@@ -14,7 +21,7 @@
             <label for="xinglink">Link zum Xing-Profil</label>
         </div>
         <div class="col-sm-6 col-md-6">
-            <input type="text" class="form-control" id="xinglink" name="xinglink" placeholder="Link eintragen">
+            <input type="text" class="form-control" id="xinglink" name="xinglink" placeholder="Link eintragen" value="{{$fmXingLink}}">
         </div>
         <div class="col-sm-2 col-md-2">
             <button type="submit" class="btn btn-primary">
