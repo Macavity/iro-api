9a8877c6ab2caabbe195b1f09448fe15f8d040f4
Alexander Pape <a.pape@paneon.de>
2014-04-15 23:58:58 +0200

    Change to "production"

--- app/config/xing.php
+++ app/config/xing.php
@@ -2,8 +2,8 @@
 
 return array(
 
-    'consumer_key' => 'fbb32757b4e2dab792a8',
+    'consumer_key' => '198b84112d109893c972',
 
-    'consumer_secret' => 'c9a177434719ec2eab49765b9e6fe571c76f3661',
+    'consumer_secret' => '4410fea1372b8971b4523044befbc5d516438363',
 
 );
