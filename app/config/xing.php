15ddf53af20399746ecaa52f2f98bf5549f1a3a7
Alexander Pape <a.pape@paneon.de>
2014-04-22 22:28:48 +0200

    Some bug fixes in case of an xing authentification error

--- app/config/xing.php
+++ app/config/xing.php
@@ -1,9 +1,22 @@
 <?php
 
-return array(
+if(false){
+    // Production
+    return array(
 
-    'consumer_key' => '198b84112d109893c972',
+        'consumer_key' => '198b84112d109893c972',
 
-    'consumer_secret' => '4410fea1372b8971b4523044befbc5d516438363',
+        'consumer_secret' => '4410fea1372b8971b4523044befbc5d516438363',
 
-);
+    );
+}
+else{
+    // Test
+    return array (
+
+        'consumer_key' => 'fbb32757b4e2dab792a8',
+
+        'consumer_secret' => 'c9a177434719ec2eab49765b9e6fe571c76f3661',
+
+    );
+}
