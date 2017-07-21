3d92ab0bb2c0b535771db42c20f489edd70262e0
Alexander Pape <alexander.pape@myposter.de>
2017-07-21 13:49:18 +0200

    Add shortcuts to api endpoint AlgoliaController@checkCache

--- app/views/clients/show.blade.php
+++ app/views/clients/show.blade.php
@@ -1,19 +1,60 @@
+<?php
+    /** @var $client Client */
+?>
 @section('content')
-<h1>Kundendetails</h1>
+    <h1>Kundendetails {{ $client->name }}</h1>
 
-<div class="jumbotron text-center">
-    <h2>{{ $client->name }}</h2>
-    <p>
-        @foreach($client->getFieldLabels() as $field => $label)
-        <strong>{{$label}}:</strong> {{$client->$field}}<br/>
-        @endforeach
-    </p>
-    <p>Cache für Jobliste ist
-        @if($cacheJoblistActive)
-        <span class="label label-success">Aktiv</span>
-        @else
-        <span class="label label-warning">Leer</span>
-        @endif
-    </p>
-</div>
+    <div class="panel panel-default">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th colspan="2">Stammdaten</th>
+                </tr>
+            </thead>
+            @foreach($client->getFieldLabels() as $field => $label)
+                <tr>
+                    <td><strong>{{$label}}</strong></td>
+                    <td>{{$client->$field}}</td>
+                </tr>
+            @endforeach
+            <tr>
+                <td>Cache für Jobliste ist</td>
+                <td>
+                    @if($cacheJoblistActive)
+                        <span class="label label-success">Aktiv</span>
+                    @else
+                        <span class="label label-warning">Leer</span>
+                    @endif
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="panel panel-default">
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th>API Endpoints</th>
+            </tr>
+            </thead>
+            <tr>
+                <td>Search: Check Cache (Änderungen)</td>
+                <td>
+                    <a href="{{ action('AlgoliaController@checkCache', ['serial' => $client->serial]) }}" target="_blank">Normal</a>
+                </td>
+                <td>
+                    <a href="{{ action('AlgoliaController@checkCache', ['serial' => $client->serial]) }}?debug=true" target="_blank">Debug</a>
+                </td>
+            </tr>
+            <tr>
+                <td>Search: Check Cache (Alle offenen Jobs)</td>
+                <td>
+                    <a href="{{ action('AlgoliaController@checkCache', ['serial' => $client->serial, 'type' => 'open']) }}" target="_blank">Normal</a>
+                </td>
+                <td>
+                    <a href="{{ action('AlgoliaController@checkCache', ['serial' => $client->serial, 'type' => 'open']) }}?debug=true" target="_blank">Debug</a>
+                </td>
+            </tr>
+        </table>
+    </div>
 @stop
